34fac3c4716384fa90a6869005e1ee45d058b659
ViktorStoichev <viktor.stoichev888@abv.bg>
2024-09-22 17:31:43 +0300

    Add handlebars and render first page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 const port = 5000;
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-    res.send('It  works!');
+    res.render('home');
 });
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`));
